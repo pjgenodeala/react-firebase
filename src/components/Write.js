7e89fe3dc5cbe0d0d18f906f7b97f69948b67e88
Precious <pj@LAPTOP-HMAMUB6I.>
2025-05-05 12:49:59 +0800

    writing data saved in firebase

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import app from "../firebaseConfig";
+import {getDatabase, ref, set, push} from "firebase/database";
+import {useNavigate} from 'react-router-dom';
 
 function Write() {
+    const navigate = useNavigate();
+
+    let [inputValue1, setInputValue1] = useState("");
+    let [inputValue2, setInputValue2] = useState("");
+
+    const saveData = async () => {
+        const db = getDatabase(app);
+        const newDocRef = push(ref(db, "nature/fruits"));
+        set(newDocRef, {
+            fruitname: inputValue1,
+            fruitDefinition: inputValue2
+        }).then( () => {
+            alert("data saved successfully")
+        }).catch((error) => {
+            alert("error: ", error.message);
+        })
+    }
     return (
-        <div>Write</div>
+        <div>
+            <h1>WRITE/HOMEPAGE</h1>
+            <input type="text" value={inputValue1}
+            onChange={(e) => setInputValue1(e.target.value)} />
+             <input type="text" value={inputValue2}
+             onChange={(e) => setInputValue2(e.target.value)} />
+
+             <button onClick={saveData}>Save Data</button>
+             <br />
+             <br />
+             <br />
+             <button className="button1" onClick={ () => navigate("/updateread")}>Go Update read</button>
+             <button className="button1" onClick={ () => navigate("/read")}>Go read page</button>
+        </div>
 )
 }
 
